39a69c1ecbb9e0aeab64fa39163b4d27a3b3e802
Oliver Godwin <oliver.m.godwin@gmail.com>
2022-04-03 17:27:56 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+
+app.listen(3000, () => {
+  console.log("App is running")
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!")
+});
+
+const Discord = require("discord.js")
+const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
+client.on("messageCreate", message => {
+  if(message.content === "ping") {
+    message.reply("pong")
+  }
+
+  if(message.content === "!lfg") {
+    let embed = new Discord.MessageEmbed()
+    .setAuthor(message.author.username, message.author.displayAvatarURL())
+    .setTitle("Welcome to the Orbit LFG platform")
+    .setDescription("Please select an option")
+    .setColor("#ffa500")
+    
+    message.channel.send( {embeds : [embed], components : [{type : 1, components : [{
+            type: 2,
+            label: 'Create LFG Listing',
+            style: 1,
+            customId: 'newPostMenu'
+        },
+        {
+            type: 2,
+            label: 'View Current Listings',
+            style: 2,
+            customId: 'viewPostMenu'
+        }]}] } )
+  }
+});
+
+const mySecret = process.env['token']
+
+
+client.login(mySecret);
